727f0eec6e897fb703bd9e20d6ace05a3e53cb46
Kpanchani <krunalpanchani5@gmail.com>
2022-05-04 14:57:21 +0100

    Tab

---
+++ src/Table2.java
@@ -0,0 +1,15 @@
+public class Table2 {
+    public static void main(String args[]){
+
+        //print the number between 1 to 10(including) which are divisable by 3
+        for(int i= 2; i<=2; i++){
+
+            for(int j = 1; j<=10; j++){
+                System.out.println(i + "* "+j+" = "+i*j);
+
+                }
+            }
+        }
+    }
+
+
